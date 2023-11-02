79714392a8d4328c9ac8c66f291466023d8fbf9b
Danielle <sk.dannisummers@gmail.com>
2023-11-02 14:51:06 +0000

    Project Setup

---
+++ HolidaySearchFeature.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HolidaySearchFeature.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}

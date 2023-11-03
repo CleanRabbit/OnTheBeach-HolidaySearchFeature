a6bf4931bf8612bacac2180887e7b504d12b5941
Danielle <sk.dannisummers@gmail.com>
2023-11-03 10:46:10 +0000

    Final pass
    Flipped a lot of conditional statements to make them easier to read
    Added support for specifying 'Any Airport' or 'Any London Airport'
    All tests pass

--- HolidaySearchFeature/FindMyHoliday.cs
+++ HolidaySearchFeature/FindMyHoliday.cs
@@ -15,18 +15,22 @@ namespace HolidaySearchFeature
 
 		public IEnumerable<HolidaySearchResult> Search(HolidayCriteria criteria)
 		{
+			if (criteria.DepartingFrom.Equals("Any Airport"))
+				criteria.DepartingFrom = string.Empty;	//empty field is handled like 'any airport'
+
 			//We're matching each criteria item relevant to the hotel selection, and assuming it's a match if nothing was specified
 			var hotelsMatchingCriteria = Hotels
-				.Where(h => !string.IsNullOrWhiteSpace(criteria.HotelName) ? h.Name.Equals(criteria.HotelName) : true)
-				.Where(h => !string.IsNullOrWhiteSpace(criteria.TravelingTo) ? h.Local_Airports.Contains(criteria.TravelingTo) : true)
-				.Where(h => criteria.DepartureDate != null ? h.Arrival_Date == criteria.DepartureDate : true)
-				.Where(h => criteria.Duration != 0? h.Nights == criteria.Duration : true);
+				.Where(h => string.IsNullOrWhiteSpace(criteria.HotelName) || h.Name.Equals(criteria.HotelName))
+				.Where(h => string.IsNullOrWhiteSpace(criteria.TravelingTo) || h.Local_Airports.Contains(criteria.TravelingTo))
+				.Where(h => criteria.DepartureDate == null || h.Arrival_Date == criteria.DepartureDate)
+				.Where(h => criteria.Duration == 0 || h.Nights == criteria.Duration);
 
 			//We're matching each criteria item relevant to the flight selection, and assuming it's a match if nothing was specified
 			var flightsMatchingCriteria = Flights
-				.Where(f => !string.IsNullOrWhiteSpace(criteria.DepartingFrom) ? f.From.Equals(criteria.DepartingFrom) : true)
-				.Where(f => !string.IsNullOrWhiteSpace(criteria.TravelingTo) ? f.To.Equals(criteria.TravelingTo) : true)
-				.Where(f => criteria.DepartureDate != null ? f.Departure_Date == criteria.DepartureDate : true);
+				.Where(f => criteria.DepartingFrom.Equals("Any London Airport") ? f.From.Equals("LGW") || f.From.Equals("LTN") :
+							string.IsNullOrWhiteSpace(criteria.DepartingFrom) || f.From.Equals(criteria.DepartingFrom))
+				.Where(f => string.IsNullOrWhiteSpace(criteria.TravelingTo) || f.To.Equals(criteria.TravelingTo))
+				.Where(f => criteria.DepartureDate == null || f.Departure_Date == criteria.DepartureDate);
 
 			//We've now got a list of hotels and flights that match the given criteria.
 			//Problem here is we don't truly know if we were given an open list of flights or an open list of hotels (or worse, both)
